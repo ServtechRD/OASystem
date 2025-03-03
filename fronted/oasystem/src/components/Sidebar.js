c29929ddf5e0291bec6e7ddb049c326e16ab807d
jason <awkwardkao@gmail.com>
2025-03-03 13:58:18 +0800

    add logout to sidebar

--- fronted/oasystem/src/components/Sidebar.js
+++ fronted/oasystem/src/components/Sidebar.js
@@ -1,43 +1,54 @@
-import React from 'react';
+import React from "react";
 import {
   Drawer,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
-  Box
-} from '@mui/material';
+  Box,
+  Divider,
+} from "@mui/material";
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from "react-router-dom";
 
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
-import PeopleIcon from '@mui/icons-material/People';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-
+import PeopleIcon from "@mui/icons-material/People";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const drawerWidth = 240;
 
 const Sidebar = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    // 清除認證信息
+    localStorage.removeItem("authToken");
+
+    // 導向登入頁面
+    history.push("/login");
+  };
+
   return (
     <Drawer
       variant="permanent"
       sx={{
         width: drawerWidth,
         flexShrink: 0,
-        '& .MuiDrawer-paper': {
+        "& .MuiDrawer-paper": {
           width: drawerWidth,
-          boxSizing: 'border-box',
+          boxSizing: "border-box",
         },
       }}
     >
-      <Box sx={{ overflow: 'auto', mt: 8 }}>
+      <Box sx={{ overflow: "auto", mt: 8 }}>
         <List>
-         <ListItem button component={Link} to="/employees">
-         <ListItemIcon>
-             <PeopleIcon />
-         </ListItemIcon>
-         <ListItemText primary="員工管理" />
+          <ListItem button component={Link} to="/employees">
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="員工管理" />
           </ListItem>
           <ListItem button component={Link} to="/">
             <ListItemIcon>
@@ -48,12 +59,21 @@ const Sidebar = () => {
         </List>
 
         <ListItem button component={Link} to="/leave-records">
-         <ListItemIcon>
-          <FormatListBulletedIcon />
-         </ListItemIcon>
-         <ListItemText primary="請假記錄" />
+          <ListItemIcon>
+            <FormatListBulletedIcon />
+          </ListItemIcon>
+          <ListItemText primary="請假記錄" />
         </ListItem>
+        {/* 添加分隔線 */}
+        <Divider sx={{ mt: 2, mb: 2 }} />
 
+        {/* 登出按鈕 */}
+        <ListItem button onClick={handleLogout}>
+          <ListItemIcon>
+            <LogoutIcon color="error" />
+          </ListItemIcon>
+          <ListItemText primary="登出" sx={{ color: "error.main" }} />
+        </ListItem>
       </Box>
     </Drawer>
   );
